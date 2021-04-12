b135045dfda7f4809bae67c2d49530d6ea4896d6
谷进龙 <1435751038@qq.com>
2021-04-12 17:33:33 +0800

    实时展示组件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,16 @@
 module.exports = {
     title: '公司文档',
     base: '/vuepresstest/',
-    plugins: ['demo-container'],
+    plugins:{
+      'demo-container':{
+
+      },
+      run: {
+        jsLabs: ['https://unpkg.com/element-ui/lib/index.js'],
+        cssLabs: ['https://unpkg.com/element-ui/lib/theme-chalk/index.css'],
+        reverse: true,
+    }
+    },
     extraWatchFiles: [
         '.vuepress/components/*', // 使用相对路径
       ]
--- docs/README.md
+++ docs/README.md
@@ -26,3 +26,22 @@ export default {
 </style>
 ```
 :::
+
+
+
+这里是实时展示组件
+
+
+
+::: run
+```html
+<template>
+    <el-tabs type="border-card">
+        <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+        <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+    </el-tabs>
+</template>
+```
+
